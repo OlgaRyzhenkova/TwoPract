22976a3c64d7e0a98b2322c3d2c5e7ae3626cb4f
Ryzhenkova Olga <ryzhenkova.olha1124@vu.cdu.edu.ua>
2025-11-30 22:13:00 +0200

    Добавьте файлы проекта.

---
+++ One/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Data;
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("Масив:");
+            int[,] arr = RandomInput();
+            ShowArr(arr);
+            Console.WriteLine($"Кількість чисел в діапазоні:{CountNum(arr)}");
+            Kratni(arr);
+        }
+    }
+    static int[,] RandomInput()
+    {
+        Random rand = new Random();
+        int[,] arr = new int[10,10];
+        for ( int i = 0; i < 10; i++)
+        {
+            for ( int j = 0; j < 10; j++)
+            {
+                arr[i, j] = rand.Next(10, 100);
+            }
+        }
+        return arr;
+    }
+    static void ShowArr( int[,] arr)
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                Console.Write( arr[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+    static int CountNum( int[,] arr )
+    {
+        Console.WriteLine("Введіть нижню межу:");
+        int min = Convert.ToInt32( Console.ReadLine() );
+        Console.WriteLine("Введіть верхню межу:");
+        int max = Convert.ToInt32( Console.ReadLine() );
+        int count = 0;
+        for (int i = 0; i < 10; i ++)
+        {
+            for(int j = 0; j < 10; j++)
+            {
+                if( arr[i, j] > min && arr[i, j] < max)
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    static int Kratni(int[,] arr)
+    {
+        int count = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            for (int j = 0; j < 10; j++)
+            {
+                if (arr[i, j] % 5 == 0)
+                {
+                    count++;
+                    Console.Write(arr[i, j] + " ");
+                }
+            }
+            
+        }
+        Console.WriteLine(count);
+        return count;
+    }
+}
