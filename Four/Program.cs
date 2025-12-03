98d645dab6112a140889401f5feba2a0fa98e099
Ryzhenkova Olga <ryzhenkova.olha1124@vu.cdu.edu.ua>
2025-12-03 16:25:59 +0200

    додала

---
+++ Four/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        int[] arr = CreateArray();
+        RandArray(arr);
+        PrintArray(arr);
+        int startIndex = FindMaxSum(arr, out int maxSum);
+        PrintResult(startIndex, maxSum);
+    }
+    static int[] CreateArray()
+    {
+        return new int[20];
+    }
+    static void RandArray(int[] arr)
+    {
+        Random rand = new Random();
+        for (int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = rand.Next(10, 100);
+        }
+
+    }
+    static void PrintArray(int[] arr)
+    {
+        Console.WriteLine("Масив:");
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.WriteLine(arr[i] + " ");
+        }
+        Console.WriteLine();
+    }
+    static int FindMaxSum(int[] arr, out int maxSum)
+    {
+        maxSum = -1;
+        int bestIndex = 0;
+        for (int i = 0; i < arr.Length - 2; ++i)
+        {
+            int sum = arr[i] + arr[i + 1] + arr[i + 2];
+            if (sum > maxSum)
+            {
+                maxSum = sum;
+                bestIndex = i;
+            }
+        }
+        return bestIndex;
+    }
+    static void PrintResult(int startIndex, int maxSum)
+    {
+        Console.WriteLine("Підмасив з найбільшою сумою:");
+        Console.WriteLine($"Індекси: {startIndex}, {startIndex + 1}, {startIndex + 2}");
+        Console.WriteLine($"Сума = {maxSum}");
+    }
+}
---
+++ Three/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введіть рядок анлгійською мовою:");
+        string text = Console.ReadLine();
+        int[] counts = CountLetters(text);
+        PrintCounts(counts);
+
+    }
+    static int[] CountLetters(string text)
+    {
+        int[] count = new int[26];
+        foreach (char c in text)
+        {
+            if (char.IsLetter(c))
+            {
+                char letter = char.ToLower(c);
+                int index = letter - 'a';
+                if (index >= 0 && index < 26)
+                {
+                    count[index]++;
+                }
+            }
+        }
+        return count;
+    }
+    static void PrintCounts(int[] counts)
+    {
+        Console.WriteLine("Кількість кожної букви:");
+        for (int i = 0; i < 26; i++)
+        {
+            if (counts[i] > 0)
+            {
+                char letter = (char)('a' + i);
+                Console.WriteLine($"{letter} : {counts[i]}");
+            }
+        }
+    }
+    
+}
